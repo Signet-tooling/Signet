e4026cbaf401271fed520db6a12c88a7582ee240
Lewechi <praise44224422@gmail.com>
2026-03-17 22:23:53 +0100

    feat: react native setup

---
+++ examples/react-native-demo/node_modules/@signet-sdk/react-native/node_modules/@signet-sdk/core/src/index.ts
@@ -0,0 +1,2 @@
+export const coreVersion = "0.1.0";
+export const ping = () => "pong";
---
+++ examples/react-native-demo/node_modules/@signet-sdk/react-native/src/index.ts
@@ -0,0 +1,3 @@
+import { coreVersion } from "@signet-sdk/core";
+export const rnVersion = "0.1.0";
+export const initRN = () => console.log("Init RN Core:", coreVersion);
